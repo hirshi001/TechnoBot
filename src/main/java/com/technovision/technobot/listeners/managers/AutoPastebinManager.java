4c6f6dcb05a4af1c8fb93655e55db6c57aaaa9bb
Modder <60491189+tap6386@users.noreply.github.com>
2021-01-01 14:27:06 -0800

    Fix nullpointer error message spam

--- src/main/java/com/technovision/technobot/listeners/managers/AutoPastebinManager.java
+++ src/main/java/com/technovision/technobot/listeners/managers/AutoPastebinManager.java
@@ -1,6 +1,7 @@
 package com.technovision.technobot.listeners.managers;
 
 import com.technovision.technobot.TechnoBot;
+import com.technovision.technobot.logging.Logger;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
@@ -29,47 +30,51 @@ public class AutoPastebinManager extends ListenerAdapter {
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
         if (!event.getAuthor().isBot()) {
             if (!event.getMessage().getAttachments().isEmpty()) {
-                Message.Attachment logOrJavaFile = event.getMessage().getAttachments().stream()
-                        .filter(attachment -> attachment.getFileExtension().equals("log") ||
-                                              attachment.getFileExtension().equals("java") ||
-                                              attachment.getFileExtension().equals("txt") ||
-                                              attachment.getFileExtension().equals("kt") ||
-                                              attachment.getFileExtension().equals("json") ||
-                                              attachment.getFileExtension().equals("gradle")
-                        )
-                        .findFirst()
-                        .orElseThrow(() ->
-                                new NullPointerException("Couldn't find a recognizable code or log file."));
-
-                GitHubClient gitHubClient = new GitHubClient();
-                gitHubClient.setOAuth2Token(GITHUB_TOKEN);
-
-                Gist gist = new Gist();
-
                 try {
-                    GistFile gistFile = new GistFile();
-                    String stringifiedFile = IOUtils.toString(logOrJavaFile.retrieveInputStream().get(), Charset.defaultCharset());
-                    gistFile.setContent(!stringifiedFile.isEmpty() ? stringifiedFile : "idk why this file is empty.");
+                    Message.Attachment logOrJavaFile = event.getMessage().getAttachments().stream()
+                            .filter(attachment -> attachment.getFileExtension().equals("log") ||
+                                    attachment.getFileExtension().equals("java") ||
+                                    attachment.getFileExtension().equals("txt") ||
+                                    attachment.getFileExtension().equals("kt") ||
+                                    attachment.getFileExtension().equals("json") ||
+                                    attachment.getFileExtension().equals("gradle")
+                            )
+                            .findFirst()
+                            .orElseThrow(() ->
+                                    new NullPointerException("Couldn't find a recognizable code or log file."));
 
-                    gist.setFiles(Collections.singletonMap(logOrJavaFile.getFileName(), gistFile));
-                    gist.setDescription("My stuff");
+                    GitHubClient gitHubClient = new GitHubClient();
+                    gitHubClient.setOAuth2Token(GITHUB_TOKEN);
 
-                    GistService gistService = new GistService();
-                    gistService.getClient().setOAuth2Token(GITHUB_TOKEN);
+                    Gist gist = new Gist();
 
-                    gist = gistService.createGist(gist);
+                    try {
+                        GistFile gistFile = new GistFile();
+                        String stringifiedFile = IOUtils.toString(logOrJavaFile.retrieveInputStream().get(), Charset.defaultCharset());
+                        gistFile.setContent(!stringifiedFile.isEmpty() ? stringifiedFile : "idk why this file is empty.");
 
-                } catch (InterruptedException | ExecutionException | IOException e) {
-                    e.printStackTrace();
+                        gist.setFiles(Collections.singletonMap(logOrJavaFile.getFileName(), gistFile));
+                        gist.setDescription("My stuff");
+
+                        GistService gistService = new GistService();
+                        gistService.getClient().setOAuth2Token(GITHUB_TOKEN);
+
+                        gist = gistService.createGist(gist);
+
+                    } catch (InterruptedException | ExecutionException | IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    MessageEmbed embed = new EmbedBuilder()
+                            .setTitle("Send in GitHub Gist!")
+                            .addField("We prefer not to download files, so we've made a Gist for you!", gist.getHtmlUrl(), false)
+                            .addField("WARNING", "Please refrain from sending files in the future, and use Pastebin or GH Gists instead.", false)
+                            .build();
+
+                    event.getChannel().sendMessage(embed).queue();
+                } catch (NullPointerException e) {
+                    TechnoBot.logger.log(Logger.LogLevel.INFO, e.getMessage());
                 }
-
-                MessageEmbed embed = new EmbedBuilder()
-                        .setTitle("Send in GH Gist!")
-                        .addField("We prefer not to download files, so we've made a Gist for you!", gist.getHtmlUrl(), false)
-                        .addField("WARNING", "Please refrain from sending files in the future, and use Pastebin or GH Gists instead.", false)
-                        .build();
-
-                event.getChannel().sendMessage(embed).queue();
             }
         }
     }
